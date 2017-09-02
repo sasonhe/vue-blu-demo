d241600713cc6bc1a679c40b193facd2bf0cb13a
sasonhe <642139009@qq.com>
2017-09-03 01:36:02 +0800

    add

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,15 +1,221 @@
 <template>
-<div class="">
-    <h1>hello vue</h1>
-</div>
-</template>
-<script>
-export default{
-    components:{
+  <div class="">
+      <button class="button is-primary" @click="fade">fade</button>
+      <button class="button is-primary" @click="fadeRight">fadeRight</button>
+      <button class="button is-primary" @click="fadeLeft">fadeLeft</button>
+      <button class="button is-primary" @click="fadeLeftBig">fadeLeftBig</button>
+      <button class="button is-primary" @click="fadeUpBig">fadeUpBig</button>
 
-    }
+      <br><br>
+      <button class="button is-primary" @click="rotate">rotate</button>
+      <button class="button is-primary" @click="rotateDownLeft">rotateDownLeft</button>
+      <button class="button is-primary" @click="rotateDownRight">rotateDownRight</button>
+      <button class="button is-primary" @click="rotateUpLeft">rotateUpLeft</button>
+
+      <br><br>
+      <button class="button is-primary" @click="bounce">bounce</button>
+      <button class="button is-primary" @click="bounceDown">bounceDown</button>
+      <button class="button is-primary" @click="bounceLeft">bounceLeft</button>
+      <button class="button is-primary" @click="bounceRight">bounceRight</button>
+      <button class="button is-primary" @click="bounceUp">bounceUp</button>
+
+      <br><br>
+      <button class="button is-primary" @click="slideDown">slideDown</button>
+      <button class="button is-primary" @click="slideLeft">slideLeft</button>
+      <button class="button is-primary" @click="slideRight">slideRight</button>
+      <button class="button is-primary" @click="slideUp">slideUp</button>
+
+      <br><br>
+      <button class="button is-primary" @click="zoom">zoom</button>
+      <button class="button is-primary" @click="zoomDown">zoomDown</button>
+      <button class="button is-primary" @click="zoomLeft">zoomLeft</button>
+      <button class="button is-primary" @click="zoomRight">zoomRight</button>
+      <button class="button is-primary" @click="zoomUp">zoomUp</button>
+  </div>
+</template>
+
+<script>
+export default {
 }
 </script>
-<style scoped>
 
+<style lang="css">
 </style>
+
+
+<script>
+  export default {
+    methods: {
+      fadeNotify() {
+        this.$notify.open({content: 'fade', transition: 'fade'});
+      },
+      fadeRight() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'fadeRight',
+          content: 'fadeRight...'
+        });
+      },
+      fadeLeft() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'fadeLeft',
+          content: 'fadeLeft...'
+        });
+      },
+      fadeLeftBig() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'fadeLeftBig',
+          content: 'fadeLeftBig...'
+        });
+      },
+      fadeUpBig() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'fadeUpBig',
+          content: 'fadeUpBig...'
+        });
+      },
+      fade() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'fade',
+          content: 'fade...'
+        });
+      },
+
+      rotate() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'rotate',
+          content: 'rotate...'
+        });
+      },
+      rotateDownLeft() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'rotateDownLeft',
+          content: 'rotateDownLeft...'
+        });
+      },
+      rotateDownRight() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'rotateDownRight',
+          content: 'rotateDownRight...'
+        });
+      },
+      rotateUpLeft() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'rotateUpLeft',
+          content: 'rotateUpLeft...'
+        });
+      },
+
+      bounce() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'bounce',
+          content: 'bounce...'
+        });
+      },
+      bounceDown() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'bounceDown',
+          content: 'bounceDown...'
+        });
+      },
+      bounceLeft() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'bounceLeft',
+          content: 'bounceLeft...'
+        });
+      },
+      bounceRight() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'bounceRight',
+          content: 'bounceRight...'
+        });
+      },
+      bounceUp() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'bounceUp',
+          content: 'bounceUp...'
+        });
+      },
+
+      slideDown() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'slideDown',
+          content: 'slideDown...'
+        });
+      },
+      slideLeft() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'slideLeft',
+          content: 'slideLeft...'
+        });
+      },
+      slideRight() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'slideRight',
+          content: 'slideRight...'
+        });
+      },
+      slideUp() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'slideUp',
+          content: 'slideUp...'
+        });
+      },
+
+      zoom() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'zoom',
+          content: 'zoom...'
+        });
+      },
+      zoomDown() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'zoomDown',
+          content: 'zoomDown...'
+        });
+      },
+      zoomLeft() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'zoomLeft',
+          content: 'zoomLeft...'
+        });
+      },
+      zoomRight() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'zoomRight',
+          content: 'zoomRight...'
+        });
+      },
+      zoomUp() {
+        this.$modal.open({
+          autoClose: 3,
+          transition: 'zoomUp',
+          content: 'zoomUp...'
+        });
+      },
+
+
+    },
+  };
+</script>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-
+"use strict"
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
-// import VueBlu from 'vue-blu'
+import VueBlu from 'vue-blu'
 import App from './App.vue'
-import routes from './router.config.js'
+import routes from './router.config'
+import 'vue-blu/dist/css/vue-blu.min.css'
 
-// Vue.use(VueBlu)
-Vue.sue(VueRouter)
+Vue.use(VueBlu)
+Vue.use(VueRouter)
 Vue.use(VueResource)
 
 var router = new VueRouter({
